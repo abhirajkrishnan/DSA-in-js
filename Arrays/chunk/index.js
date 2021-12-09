d33f687c33686a34ac78af7f0413099616c17d1f
abhirajkrishnan <43444088+abhirajkrishnan@users.noreply.github.com>
2021-12-10 00:48:38 +0530

    Array probs

---
+++ Arrays/chunk/index.js
@@ -0,0 +1,24 @@
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk(array, size) {
+    let chunked=[];
+    let copy=array;
+    
+    while(copy.length>0){
+        let part=copy.slice(0,size);
+        copy.splice(0,size)
+        chunked.push(part)
+    }
+
+    return chunked;
+}
+
+module.exports = chunk;
