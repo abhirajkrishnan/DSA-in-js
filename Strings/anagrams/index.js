80c113f6c4796ec7113c7aa2f2d47dbabe788860
abhirajkrishnan <43444088+abhirajkrishnan@users.noreply.github.com>
2021-12-10 00:48:51 +0530

    strings probs

---
+++ Strings/anagrams/index.js
@@ -0,0 +1,46 @@
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+
+function charCount(str){
+
+    return str.split("").reduce((acc,val)=>{
+        acc[val]=(acc[val] || 0) +1;
+            return acc;
+    },{})
+}
+
+function cleanString(str){
+    return str.replace(/[^\w]/g,'').toLowerCase().split('').sort().join("");
+}
+   
+
+
+// function anagrams(stringA, stringB) {
+//     let objA={};
+//     let objB={};
+
+//     objA=charCount(stringA);
+//     objB=charCount(stringB);
+
+//     if(Object.keys(objA).length!=Object.keys(objB).length) return false;
+
+//     for (key in objA){
+//         if(objA[key]!==objB[key]) return false;
+//     }
+
+//     return true;
+// }
+
+function anagrams(stringA,stringB){
+    return cleanString(stringA)===cleanString(stringB)
+}
+
+module.exports = anagrams;
